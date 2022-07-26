19db20b5558aca4c9d9027fd9e662421abf68871
sadlerb <sadlerb22@gmail.com>
2022-07-25 22:01:42 -0500

    Added hotzone functionality

--- app/services/hotzones.services.js
+++ app/services/hotzones.services.js
@@ -3,7 +3,7 @@ const pool = require('../services/db')
 
 // get all
 exports.getAllHotzones=(req,res) => {
-    const query = "SELECT * from hotzones"
+    const query = "CALL getAllHotzoneDetails()"
     pool.query(query,(err,results)=>{
         if (err){
             res.json(err)
@@ -15,7 +15,7 @@ exports.getAllHotzones=(req,res) => {
 }
 // get one 
 exports.getHotzone = (req,res) => {
-    const query = `Select * from hotzones WHERE hotzone_id = ${req.params.id}`
+    const query = `CALL getHotzoneDetails(${req.params.id})`
     pool.query(query,(err,results) => {
         if (err) {
             res.json(err)
@@ -35,7 +35,7 @@ exports.getHotzone = (req,res) => {
 // create
 exports.createHotzone = (req,res) => {
     const {severity_rating,size} = req.body
-    const query = `INSERT INTO hotzones (severity_rating,size) VALUES (${severity_rating},${size}})`
+    const query = `INSERT INTO hotzones (severity_rating,size) VALUES (${severity_rating},${size})`
     pool.query(query,(err) => {
         if (err){
             res.json(err)
@@ -51,13 +51,18 @@ exports.setHotzoneLocation = (req,res) => {
     const hotzone_id = req.params.id
     const {latitude,longitude} = req.body
     const query = `CALL setHotzoneLocation(${hotzone_id},${latitude},${longitude})`
+    console.log("BEFORE START")
     pool.query = (query,(err) => {
+        console.log("QUERY START")
         if (err){
+            console.log(err)
             res.json(err)
             return
         }
+        
         res.status(200)
-        res.json({"message":"hotzone sucessfully added"})
+        res.json({"message":"hotzone location added"})
+        return
     })
 }
 
