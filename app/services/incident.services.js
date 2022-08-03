be8fbb30ca9e742b511c164b5d4ed1a0e635c35c
sadlerb <sadlerb22@gmail.com>
2022-08-02 20:22:21 -0500

    Added incident heuristic update

--- app/services/incident.services.js
+++ app/services/incident.services.js
@@ -1,5 +1,6 @@
 const pool = require('../services/db')
-
+const geolib = require ('geolib')
+const { forEach } = require('async')
 
 
 exports.getIncidents = (req,res) =>{
@@ -31,17 +32,49 @@ exports.getOneIncident = (req,res) => {
 }
 
 exports.createIncident = (req,res) => {
-    const {location,severity,date,report_details} = req.body
-    const {latitude,longitude} = location
+    const {latitude,longitude,severity,date,report_details,heuristic} = req.body
 
-    const query = `CALL addIncident(${latitude},${longitude},${severity},'${date}','${report_details}')`
+    addIncident(latitude,longitude,severity,date,report_details,heuristic)
+
+    //Immediately returns a message to frontend. Database updates in background
+    res.json({"message":"INCIDENT ADDED. UPDATING DATABASE"})
+    
+}
+
+function addIncident(latitude,longitude,severity,date,report_details,heuristic){
+    const max_distance = 100 // Max distance where the incident will affect the heuristics of destination 
+    const query = `CALL addIncident(${latitude},${longitude},${severity},'${date}','${report_details},${heuristic}')`
     pool.query(query,(err) => {
         if (err){
             res.json(err)
             return
         }
-        res.status(200)
-        res.json({"message": "incident successfuly created"})
-    })
-}
+        pool.query(`CALL getAllDestinationDetails()`,(err,results)=>{
+            if (err){
+                throw err
+            }
+            // Check if destination is close to incident
+            results[0].forEach((row)=>{
+                const dist = geolib.getDistance(
+                    {latitude:latitude,longitude:longitude},
+                    {latitude:row['latitude'],longitude:row['longitude']}
+                );
+                    
+                if (dist <=max_distance){
+                    const calculated_heuristic_percentage = 1 - (dist/max_distance)
+                    const heuristic_calculated = Math.floor(heuristic * calculated_heuristic_percentage)
+                    console.log({"name":row['name'],'heuristic':heuristic_calculated,"distance":dist})
+                    const query = `UPDATE destinations SET heuristics=${heuristic_calculated} WHERE destination_id=${row['destination_id']}`
+                    pool.query(query,(err)=>{
+                        if (err) throw err
+                    });
+                };
+                
+            });
+        });
+
+    });
+    
+     
+};
 
